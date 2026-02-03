c50f150abceeb3afd8962f52aa107f854bff36d3
Abdomo16 <abdoandali12@email.com>
2026-02-03 17:50:22 +0200

    add monthly statistics model

---
+++ lib/features/statistics/data/models/monthly_stat.dart
@@ -0,0 +1,11 @@
+class MonthlyStat {
+  final double avgCups;
+  final double completionRate;
+  final String bestDay;
+
+  MonthlyStat({
+    required this.avgCups,
+    required this.completionRate,
+    required this.bestDay,
+  });
+}
