7729318d37ee70f3c62442601194dccdea75dc33
Abdomo16 <abdoandali12@email.com>
2026-02-13 06:34:29 +0200

    add get and update methods in UserProfileRepository

--- lib/features/onboarding/data/repositories/user_profile_repository.dart
+++ lib/features/onboarding/data/repositories/user_profile_repository.dart
@@ -1,5 +1,4 @@
 import 'package:hydrowflow/database/app_database.dart';
-
 import '../models/onboarding_model.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -15,5 +14,40 @@ class UserProfileRepository {
       'daily_goal': dailyGoal,
       'onboarding_done': 1,
     }, conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<Map<String, dynamic>?> getProfile() async {
+    final db = await AppDatabase.database;
+
+    final result = await db.query(
+      'user_profile',
+      where: 'id = ?',
+      whereArgs: [1],
+    );
+
+    if (result.isEmpty) return null;
+
+    return result.first;
+  }
+
+  Future<void> updateProfile({
+    required double height,
+    required double weight,
+    required String activityLevel,
+    required double dailyGoal,
+  }) async {
+    final db = await AppDatabase.database;
+
+    await db.update(
+      'user_profile',
+      {
+        'height': height,
+        'weight': weight,
+        'activity_level': activityLevel,
+        'daily_goal': dailyGoal,
+      },
+      where: 'id = ?',
+      whereArgs: [1],
+    );
   }
 }
