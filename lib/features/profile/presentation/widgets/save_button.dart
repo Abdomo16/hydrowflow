e1ba38bc83b694f72575d28147e0cfbd11fa8d64
Abdomo16 <abdoandali12@email.com>
2026-02-18 07:27:13 +0200

    ui: compact SaveButton design

--- lib/features/profile/presentation/widgets/save_button.dart
+++ lib/features/profile/presentation/widgets/save_button.dart
@@ -4,25 +4,25 @@ class SaveButton extends StatelessWidget {
   final VoidCallback onPressed;
 
   const SaveButton({super.key, required this.onPressed});
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      height: 59,
+      height: 55,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: const Color(0xFF3A8DFF),
+          padding: const EdgeInsets.symmetric(vertical: 10),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
+            borderRadius: BorderRadius.circular(20),
           ),
         ),
         onPressed: onPressed,
         child: const Text(
           "Save Changes",
           style: TextStyle(
-            fontSize: 17,
-            fontWeight: FontWeight.w800,
+            fontSize: 15,
+            fontWeight: FontWeight.w700,
             color: Colors.white,
           ),
         ),
